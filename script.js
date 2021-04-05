5a86a671752448a28dced169e690721e7d4080a0
Tranik54 <tranik54@gmail.com>
2021-04-06 00:03:13 +0700

    fix 'var' to 'let'

---
+++ script.js
@@ -0,0 +1,11 @@
+let money = 54000;
+let income = 12000;
+let addExpenses = "Аренда, Бензин, Еда, Связь";
+let deposit = false;
+let mission = 800000;
+let period = 12;
+
+console.log(money);
+
+alert('Нажми на кнопку');
+
